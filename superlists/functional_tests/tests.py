6bc6bf403e3ee7506f3ec9545c2ea1fd904e29a3
frclasso <frcalsso@yahoo.com.br>
2020-08-02 20:05:31 -0300

    definindo metodo max wait

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -1,10 +1,12 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
 import time
 
 
 class NewVisitorTest(LiveServerTestCase):
+    MAX_WAIT = 10
 
     def setUp(self):
         self.browser = webdriver.Firefox(executable_path='/usr/local/bin/geckodriver')
@@ -26,26 +28,45 @@ class NewVisitorTest(LiveServerTestCase):
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
 
+
+        #Quando ele tecla enter, a página é atualizda, e agora a página lista
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
             inputbox.get_attribute('placeholder'), 'Enter a to-do item'
         )
-
-
-        #pag102
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys("Use peacock feathers to make a fly")
+        inputbox.send_keys("Buy peacock feathers")
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+
+        # Ainda continua havendo uma caixa de texto convidando-a a acrescentar
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys("Use peacock feathers to make a fly")
+        inputbox.send_keys(Keys.ENTER)
+
+
+        # A página é atualizada novamente e agora mostra os dois items em sua lista
+        self.wait_for_row_in_list_table('2: Use peacock feather')
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers ',[row.text for row in rows])
+        self.assertIn('1: Buy peacock feathers',[row.text for row in rows])
 
-        self.assertIn('2: Use peacock feather', [row.text for row in rows])
+        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
 
         self.fail('Finish the test!')
 
-
-
-
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row_text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > self.MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
